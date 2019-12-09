e1c2dab1d430db7a462b2017ebd798047dbe82d6
septianferry13 <42851687+septianferry13@users.noreply.github.com>
2019-12-09 12:45:08 +0700

    index.php

--- index.php
+++ index.php
@@ -145,49 +145,6 @@ else {
 
 <!-- ################################ 1 ################################ -->
 <?php if (!isset($_SESSION['msisdn']) and !isset($_SESSION['otp']) and !isset($_SESSION['secret_token']) ){ ?>
-<body>
-<!DOCTYPE html>
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html;charset=utf-8">
-<title>Hitung Mundur</title>
-<link rel="stylesheet" href="jquery.countdown.css">
-<style type="text/css">
-#hitmundur { width: 240px; height: 45px; }
-</style>
-<script src="jquery.min.js"></script>
-<script src="jquery.plugin.js"></script>
-<script src="jquery.countdown.js"></script>
-
-
-<script type="text/javascript">
-function waktuHabis(){
- alert("Waktu anda habis!!!");
- }  
-function hampirHabis(periods){
- if($.countdown.periodsToSeconds(periods) == 30){
-  $(this).css({color:"red"});
-  }
- }
-$(function(){
- 
- var sisa_waktu =  <?php echo $waktu ?>;
- 
- var TimeOut = sisa_waktu;
- $("#hitmundur").countdown({
-  until: TimeOut,
-  compact:true,
-  onExpiry:waktuHabis,
-  onTick: hampirHabis
-  }); 
- })
-</script>
-
-</head>
-<body>
-<h1>Hitung Mundur</h1>
-
-<div id="hitmundur"></div>
 </body>
 </html>
 <style>
