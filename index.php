f9e29aa18e50b51f0ee23db97f8a5bf091abf469
septianferry13 <42851687+septianferry13@users.noreply.github.com>
2019-12-10 19:16:10 +0700

    index.php

--- index.php
+++ index.php
@@ -289,6 +289,16 @@ GET OTP
             USER
 </form>
 </div>
+var timeleft = 10;
+var downloadTimer = setInterval(function(){
+  document.getElementById("countdown").innerHTML = timeleft + " seconds remaining";
+  timeleft -= 1;
+  if(timeleft <= 0){
+    clearInterval(downloadTimer);
+    document.getElementById("countdown").innerHTML = "Finished"
+  }
+}, 1000);
+<div id="countdown"></div>
 </div>
 </body>
 
